eb39340881ba63800e53db696cc05fd422c14a02
soulhzl <h574114947zl@163.com>
2018-07-19 16:09:26 +0800

    update

--- reactpractice/page/src/js/pc_newscontainer.js
+++ reactpractice/page/src/js/pc_newscontainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col, Tabs, Carousel } from 'antd';
 import PcNewsBlock from './pc_news_block.js';
 import PcNewsImageBlock from './pc_news_image_block.js';
+import PCProduct from './pc_product.js';
 
 const TabPane = Tabs.TabPane;
 export default class PcNewsContainer extends React.Component{
@@ -29,11 +30,20 @@ export default class PcNewsContainer extends React.Component{
 			<PcNewsImageBlock count={6} type='guoji' width='400px' cartTitle='国际头条' imageWidth='112px'/>
 			</div>
 			</div>
-			<Tabs class='tabs_news'>
+			<div class='tabs_news'>
+			<Tabs>
 			<TabPane tab='新闻' key='1'>
-			<PcNewsBlock count={27} type='top' width='100%' bordered='false'/>
+			<PcNewsBlock count={24} type='top' width='100%' bordered='false'/>
 			</TabPane>
 			</Tabs>
+			</div>
+			<div class='tabs_product'>
+			<Tabs>
+			<TabPane tab='News' key='1'>
+			<PCProduct/>
+			</TabPane>
+			</Tabs>
+			</div>
 			<PcNewsImageBlock count={14} type='yule' width='100%' cartTitle='娱乐头条' imageWidth='132px'/>
 			</Col>
 			<Col span={2}></Col>
