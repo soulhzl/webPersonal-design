66b239aa968eff7a9ef18551a9efcf07e622d052
soulhzl <30329936+soulhzl@users.noreply.github.com>
2019-07-08 16:30:48 +0800

    update

--- 项目实战/vue-dota商城/index.js
+++ 项目实战/vue-dota商城/index.js
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const router = require('koa-router')()
 const bodyParser = require('koa-bodyparser')
 const passport = require('koa-passport')
+const cors = require('koa2-cors')
 
 const userrouter = require('./router/user')
 const goodsrouter = require('./router/goodsinfo')
@@ -15,6 +16,9 @@ app.use(bodyParser())
 
 // passport 初始化
 app.use(passport.initialize())
+
+// 跨域
+app.use(cors())
 
 // 配置passport
 require('./config/passport')(passport)
